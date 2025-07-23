59bbfd636e23756141ef9926296173cd56ca471e
amirwein <amirwein@hawaii.edu>
2025-07-23 11:20:03 -1000

    Update desynchronize.m
    comments updated

--- desynchronize.m
+++ desynchronize.m
@@ -1,5 +1,8 @@
 function [ar,ai]= desynchronize(arn1,ain1,time_axs,my_time,xlngth,ylngth, LOSS_C)
-%% Function to calculate the Radiation pulse in time and frequency domain
+%% Function to implement desynchronization 
+% walk through the matrix
+% use linear interpolation to implement the effect
+% Multiply the field by the power retained in the cavity
 
  for r=1:1:xlngth
         for c=1:1:ylngth
@@ -9,16 +12,7 @@ function [ar,ai]= desynchronize(arn1,ain1,time_axs,my_time,xlngth,ylngth, LOSS_C
             testi=squeeze(squeeze(ain1(r,c,:)));
             ainn1=interp1(time_axs,testi,my_time, 'linear', 'extrap');
             ar(r,c,:)=arnn1*LOSS_C^0.5;
-            ai(r,c,:)=ainn1*LOSS_C^0.5;
-            % if (r<20) && (c<20)
-            %     figure(4)
-            %     title([' ',num2str(r),' ',num2str(c)])
-            %     plot(squeeze(squeeze(ar1(r,c,:))))
-            %     hold on
-            %     plot(squeeze(squeeze(arn1(r,c,:))))
-            %     %pause(1)
-            %     hold off
-            % end
+            ai(r,c,:)=ainn1*LOSS_C^0.5;  
         end
     end
 end
