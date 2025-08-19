c48a757fcb940fb9d17ad1ec7a98241baa4c538f
amirwein <amirwein@hawaii.edu>
2025-08-19 10:10:52 -1000

    Update my_funct.m

--- my_funct.m
+++ my_funct.m
@@ -9,10 +9,10 @@ function [power_z,power_s,angl]= my_funct(ar,ai,i0,dx)
  power_s = sum(sum(power_s,1),2);                                          % Sum over dx dy 
  power_s = squeeze(power_s(1,1,:))*i0*dx^2;
  
- Ea=ar+1i*ai;
+ Ea=ar+1i*ai;      
  Ea= sum(sum(Ea,1),2);
  angl=squeeze(Ea(1,1,:));
- angl=unwrap(angle(angl));
+ angl=unwrap(angle(angl));                                                 % Get the Phase
  
  E=ar+1i*ai;  
  Ef=abs(fftshift(fft(E, [], 3))).^2;                                       % For Spectrum of Radiation pulse    
